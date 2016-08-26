e7b9fcde99c4ddf15f2141fbb5f1a27973f661f4
Truy Bui Ngoc <truyngoc@gmail.com>
2016-08-26 15:46:25 +0700

    Hangfire test
    Demo Hangfire test on ASP.NET

---
+++ HangfireTest/HangfireTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HangfireTest.Global" Language="C#" %>
---
+++ HangfireTest/HangfireTest/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Hangfire;
+
+[assembly: OwinStartup(typeof(HangfireTest.Startup))]
+
+namespace HangfireTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            GlobalConfiguration.Configuration
+                .UseSqlServerStorage(@"Server=.\SQL2012; Database=Hangfire; Uid=sa; Pwd=123456");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            // http://hangfire.io/
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Check bitcoin wallet at " + DateTime.Today.ToString()), Hangfire.Cron.MinuteInterval(2));
+        }
+    }
+}
